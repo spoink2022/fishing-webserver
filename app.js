9343ba0303e5f7ac7404e27ec931fa0ac50f749a
spoink2022 <57078041+spoink2022@users.noreply.github.com>
2021-04-09 22:45:33 -0400

    commands page

--- app.js
+++ app.js
@@ -1,37 +1,49 @@
+const PROD = true;
+
 const Global = require('./global.js');
 const config = require('./private/config.json');
 
 const fs = require('fs');
 const http = require('http');
 const https = require('https');
-let privateKey  = fs.readFileSync('/etc/letsencrypt/live/bigtuna.xyz/privkey.pem', 'utf8');
-let certificate = fs.readFileSync('/etc/letsencrypt/live/bigtuna.xyz/cert.pem', 'utf8');
 
-let credentials = {key: privateKey, cert: certificate};
+if (PROD) {
+  var privateKey  = fs.readFileSync('/etc/letsencrypt/live/bigtuna.xyz/privkey.pem', 'utf8');
+  var certificate = fs.readFileSync('/etc/letsencrypt/live/bigtuna.xyz/cert.pem', 'utf8');
+
+  var credentials = {key: privateKey, cert: certificate};
+}
+
 const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
-app.use(function(req, res, next) {
-    if(!req.secure) {
-      return res.redirect(['https://', req.get('Host'), req.url].join(''));
-    }
-    next();
-});
+if (PROD) {
+  app.use(function(req, res, next) {
+      if(!req.secure) {
+        return res.redirect(['https://', req.get('Host'), req.url].join(''));
+      }
+      next();
+  });
+}
 
 app.get('/', (req, res) => {
-    const stats = Global.getStats();
-    res.render('main.ejs', {fish: stats.fishCaught, weight: stats.tonsCaught});
+  const stats = Global.getStats();
+  res.render('main.ejs', {fish: stats.fishCaught, weight: stats.tonsCaught});
+});
+
+app.get('/commands', (req, res) => {
+  res.render('commands.ejs');
 });
 
 let httpServer = http.createServer(app);
-let httpsServer = https.createServer(credentials, app);
-
 httpServer.listen(config.HTTP_PORT);
 console.log(`Listening: http on port ${config.HTTP_PORT}`);    
 
-
-httpsServer.listen(config.HTTPS_PORT);
-console.log(`Listening: https on port ${config.HTTPS_PORT}`);
+if (PROD) {
+  let httpsServer = https.createServer(credentials, app);
+  httpsServer.listen(config.HTTPS_PORT);
+  console.log(`Listening: https on port ${config.HTTPS_PORT}`);
+}
---
+++ public/js/commands.js
@@ -0,0 +1,31 @@
+onload = function() {
+  document.getElementById('cover').remove();
+  let last = document.getElementsByClassName('selected')[0];
+  document.querySelectorAll('#type-selector p').forEach(element => {
+    element.addEventListener('click', event => {
+      event.target.classList.add('selected');
+      if (event.target != last) {
+        last.classList.remove('selected');
+        document.getElementById(last.innerHTML).classList.remove('visible');
+        last = event.target;
+        document.getElementById(last.innerHTML).classList.add('visible');
+
+        let selected = document.getElementsByClassName('bottom-selected')[0];
+        if (selected) {
+          selected.classList.remove('bottom-selected');
+          selected.parentElement.classList.remove('command-selected');
+        }
+      }
+    });
+  });
+}
+
+function expand(e) {
+  let command = e.parentElement;
+  let bottom = command.getElementsByClassName('bottom')[0];
+  opened = bottom;
+  if (bottom.classList.contains('bottom-selected')) { bottom.classList.remove('bottom-selected'); }
+  else { bottom.classList.add('bottom-selected'); }
+  if (command.classList.contains('command-selected')) { command.classList.remove('command-selected'); }
+  else { command.classList.add('command-selected'); }
+}
