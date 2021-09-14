80c50d23378c2d1f1f9c89b0d38739e0943a2d3e
spoink2022 <57078041+spoink2022@users.noreply.github.com>
2021-09-14 17:18:09 -0400

    debugging

--- app.js
+++ app.js
@@ -203,7 +203,6 @@ app.post('/webhook', bodyParser.raw({type: 'application/json'}), async (req, res
 // WEBSOCKET HANDLING
 io.on('connection', (socket) => {
   socket.on('catch', (data) => {
-    console.log('fish catch!');
     Global.setVariables(data.fish, data.tons);
     io.emit('catch', data);
   });
