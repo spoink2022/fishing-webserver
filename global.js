751d7e7da1669f63ff81c270636a1e6dbe8c73e9
spoink2022 <57078041+spoink2022@users.noreply.github.com>
2022-01-06 08:55:28 -0500

    asdf

--- global.js
+++ global.js
@@ -11,9 +11,11 @@ async function pullVariables() {
     fishCaught = parseInt(await db.users.fetchTotalFishCaught());
     tonsCaught = parseFloat(await db.users.fetchTotalWeightCaught());
     tonsCaught = Math.round(tonsCaught * 1000) / 1000;
+    console.log(fishCaught, tonsCaught);
 }
 
 module.exports.updateVariables = async function(fish, kg) {
+    console.log(fish, kg);
     fishCaught += fish;
     tonsCaught += kg/1000;
 }
